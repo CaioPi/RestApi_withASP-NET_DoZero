7c039eedfc4c0f52f3402851bb03b9f208f72999
Caio Pinelli <caiopinelli89@hotmail.com>
2022-04-06 01:13:20 -0300

    Testing Four Verbs

---
+++ Rest_ASP-Net_4_verbos/Rest_ASP-Net/Rest_ASP-Net/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,70 @@
+﻿using Rest_ASP_Net.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rest_ASP_Net.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> Persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                Persons.Add(person);
+            }
+            return Persons;
+        }
+
+
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Caio",
+                LastName = "Pinelli",
+                Address = "Alfenas",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name"+i,
+                LastName = "Person LastName"+i,
+                Address = "Some Address"+i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
