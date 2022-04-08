4277e90807b9084e085ddece99ca0f30db825178
Caio Pinelli <caiopinelli89@hotmail.com>
2022-04-07 21:42:43 -0300

    api conectada ao banco de dados mysql

---
+++ Rest_ASP-Net_Conectando_com_BD/Rest_ASP-Net/Rest_ASP-Net/Controllers/PersonController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rest_ASP_Net.Model;
+using Rest_ASP_Net.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_ASP_Net.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personservice;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+
+        {
+            _logger = logger;
+            _personservice = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+
+            return Ok(_personservice.FindAll());
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+
+
+            var personV = _personservice.FindById(id);
+            if (personV == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(personV);
+
+        }
+        [HttpPost]
+        public IActionResult Post_Create([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+            return Ok(_personservice.Create(person));
+
+        }
+        [HttpPut]
+        public IActionResult Put_Update([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+            return Ok(_personservice.Update(person));
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete_Delete(long id)
+        {
+
+
+            _personservice.Delete(id);
+            return NoContent();
+
+        }
+
+    }
+}
---
+++ Rest_ASP-Net_Conectando_com_BD/Rest_ASP-Net/Rest_ASP-Net/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Rest_ASP_Net.Model.Context;
+using Rest_ASP_Net.Services;
+using Rest_ASP_Net.Services.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rest_ASP_Net
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MysqlContext>(options => options.UseMySql(connection,ServerVersion.AutoDetect(connection)));
+
+            //injeção de dependencia
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
